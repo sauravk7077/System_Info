ea8865916e1443c9a1a7793395a6dd4dfd77cd03
sauravk7077 <sauravk7077@gmail.com>
2020-07-02 19:55:06 +0530

    renamed static to views, add functionality os info

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ const si = require('systeminformation');
 const express = require('express');
 
 var app = express();
+//Register the view engine
 app.set('view engine', 'ejs');
 
 
 app.listen(3000);
 
 app.get('/', (req, res)=> {
-    si.versions()
-    .then(() => {
-        res.render('./static/index', {data: "This is the data."});
+    si.osInfo()
+    .then((data) => {
+        res.render('index', {data: data});
+        console.log(data);
     })
     
 });
