091e387e9b13877d7e94b908879862473f93d66f
sauravk7077 <sauravk7077@gmail.com>
2020-07-03 14:16:17 +0530

    changed ui and added routes

--- main.js
+++ main.js
@@ -5,14 +5,39 @@ var app = express();
 //Register the view engine
 app.set('view engine', 'ejs');
 
-
+//Listening at port 3000
 app.listen(3000);
+
+//public folder is now public
+app.use(express.static('public'));
+
+
+
 
 app.get('/', (req, res)=> {
     si.osInfo()
     .then((data) => {
-        res.render('index', {data: data});
+        data.title = "Home";
+        res.render('index', {data: null});
         console.log(data);
     })
     
+});
+
+app.get('/os_info', (req, res)=>{
+    si.osInfo()
+    .then(data=>{
+        data.title = "OS Info";
+        res.render('index', {data:data});
+    });
+});
+
+app.get('/cpu_info', (req, res)=> {
+   
+    si.cpu().then(
+        data=>{
+            data.title = "CPU Info";
+            res.render('index', {data:data});
+        }
+    );
 });
